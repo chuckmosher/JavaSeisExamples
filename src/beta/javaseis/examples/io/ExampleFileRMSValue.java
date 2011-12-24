711fb07e4fda057743cccace2b9e3482f7e747e8
Chuck Mosher <chuckmosher@yahoo.com>
2011-12-24 19:02:27 +0000

    Refactor JavaSeisExamples to use default SVN repository file layout (trunk, branches, tags)

---
+++ src/beta/javaseis/examples/io/ExampleFileRMSValue.java
@@ -0,0 +1,50 @@
+package beta.javaseis.examples.io;
+
+import java.io.File;
+import java.util.Iterator;
+
+import beta.javaseis.io.Seisio;
+import beta.javaseis.parset.ParameterSetIO;
+import beta.javaseis.util.SeisException;
+
+import edu.mines.jtk.util.ParameterSet;
+
+public class ExampleFileRMSValue {
+
+  /**
+   * @param args
+   * @throws SeisException 
+   */
+  public static void main(String[] args) throws SeisException {    
+
+    // Convert arguments to a ParameterSet object for easy access
+    ParameterSet parset = ParameterSetIO.argsToParameters( args );
+    // Get path name for new dataset - default to "jsCreateTest" in User.home
+    String path = parset.getString("path", System.getProperty("user.home") + File.separator + 
+        "jsCreateTest" );
+
+    System.out.println("Calculate RMS trace value of JavaSeis dataset\nPath: " + path );
+    // Attempt to open
+    Seisio sio = new Seisio( path );
+    sio.open("r");
+    // Get the trace array and iterator for frames
+    float[][] trc = sio.getTraceDataArray();
+    Iterator<int[]> frames = sio.frameIterator();
+    double rms = 0.0;
+    double sum = 0;
+    // Loop over frames and calculate RMS value
+    while (frames.hasNext()) {
+      frames.next();
+      int ntrc = sio.getTracesInFrame();
+      for (int j=0; j<ntrc; j++) {
+        for (int i=0; i<trc[j].length; i++) {
+            sum = sum + 1;
+            rms = rms + (double)(trc[j][i]*trc[j][i]);
+        }
+      }
+    }
+    rms = Math.sqrt(rms/sum);
+    System.out.println("RMS Value of dataset = " + rms );
+  }
+
+}
---
+++ src/beta/javaseis/examples/parallel/ExampleParallelTask.java
@@ -0,0 +1,62 @@
+package beta.javaseis.examples.parallel;
+
+import java.util.concurrent.ExecutionException;
+
+import beta.javaseis.parallel.IParallelContext;
+import beta.javaseis.parallel.ParallelTask;
+import beta.javaseis.parallel.ParallelTaskExecutor;
+
+/**
+ * Example showing how the JavaSeis parallel thread model is used. The top level
+ * class has a main method that expects the number of tasks to be used supplied
+ * as the first argument to the program. If there are no arguments, one task is
+ * used. A static inner class is used to provide the worker task.
+ * 
+ */
+public class ExampleParallelTask {
+  /**
+   * Main method that creates and runs the JavaSeis ParallelTaskExecutor with
+   * simple worker task that prints out the task number from each active thread.
+   * 
+   * @param args
+   *          number of tasks to use as the first program argument (default 2).
+   * @throws ExecutionException 
+   */
+  public static void main(String[] args) throws ExecutionException {
+    // Default number of tasks to one
+    int ntask = 4;
+    // Check to see if any arguments were provided
+    if (args != null && args.length > 0) {
+      // Convert the argument to an integer if it was provided
+      ntask = Integer.parseInt(args[0]);
+    }
+    // Start the parallel task executor with the simple class provided below
+    new ParallelTaskExecutor(SimpleParallelTask.class, ntask);
+    // The version of the constructor above immediately starts all the tasks
+    // waits for all the tasks to finish, so nothing more is needed !
+  }
+
+  /**
+   * Example ParallelTask that uses an inner class (class defined only within
+   * another class). Using a static inner class lets us create it without having
+   * to refer to the outer class or have it in a separate file. This one gets
+   * the parallel context from the ParallelTask superclass, and then prints out
+   * the task number.
+   */
+  public static class SimpleParallelTask extends ParallelTask {
+    /**
+     * Classes that implement the ParallelTask interface must provide a run
+     * method. In this method we get the "communicator" from the ParallelTask
+     * superclass, and then print out the task number from each task using the
+     * serialPrint method from the communicator.
+     */
+    @Override
+    public void run() {
+      // Get the communicator from the superclass
+      IParallelContext pc = this.getParallelContext();
+      // Use the serialPrint method to output the rank of this task
+      pc.serialPrint("Yo from task " + pc.rank());
+    }
+
+  }
+}
