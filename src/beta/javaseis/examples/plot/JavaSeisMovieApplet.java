7884eacd236947bd192148299b5f008725f0a8e7
chuckmosher <chuckmosher@yahoo.com>
2012-01-02 08:37:41 -0600

    Rename PlotMovieApplet to JavaSeisMovieApplet

--- src/beta/javaseis/examples/plot/JavaSeisMovieApplet.java
+++ src/beta/javaseis/examples/plot/JavaSeisMovieApplet.java
@@ -3,18 +3,18 @@ package beta.javaseis.examples.plot;
 import javax.swing.JApplet;
 
 import beta.javaseis.plot.IMovieSource;
-import beta.javaseis.plot.PlotMovie;
+import beta.javaseis.plot.JavaSeisMovie;
 import beta.javaseis.plot.RandomMovieSource;
 
-public class PlotMovieApplet extends JApplet {
+public class JavaSeisMovieApplet extends JApplet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Create the applet.
 	 */
-	public PlotMovieApplet() {
+	public JavaSeisMovieApplet() {
 	    IMovieSource source = new RandomMovieSource(new int[] { 100, 100, 100 });
-	    PlotMovie.display(source);
+	    JavaSeisMovie.display(source);
 	}
 
 }
