ef1aa302f8d7f59db7fce6ea69ae69a5d4429007
Marcus Wilson <wilsonmr@ogeop.com>
2015-06-03 18:46:27 -0600

    Added movie-from-file constructor

--- src/org/javaseis/examples/plot/JavaSeisMovieApplet.java
+++ src/org/javaseis/examples/plot/JavaSeisMovieApplet.java
@@ -1,19 +1,35 @@
 package org.javaseis.examples.plot;
 
+import java.io.File;
+
 import javax.swing.JApplet;
 
+import org.javaseis.util.SeisException;
+
 import beta.javaseis.plot.IMovieSource;
+import beta.javaseis.plot.JSMovieSource;
 import beta.javaseis.plot.JavaSeisMovie;
 import beta.javaseis.plot.RandomMovieSource;
 
 public class JavaSeisMovieApplet extends JApplet {
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	/**
-	 * Create the applet.
-	 */
-	public JavaSeisMovieApplet() {
-	    JavaSeisMovie.display();
-	}
+  /**
+   * Create the applet with a random input
+   */
+  public JavaSeisMovieApplet() {
+    IMovieSource source = new RandomMovieSource(new int[] { 100, 100, 100 });
+    JavaSeisMovie.display(source);
+  }
+
+  /**
+   * Create the applet with input from a JavaSeis dataset on disk
+   * @param filePath
+   * @throws SeisException
+   */
+  public JavaSeisMovieApplet(String filePath) throws SeisException {
+    IMovieSource source = new JSMovieSource(new File(filePath));
+    JavaSeisMovie.display(source);
+  }
 
 }
